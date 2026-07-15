685df676d899dc7ffc4727ea586f326935a9e398
Jordan Benson <bensonj144@gmail.com>
2026-07-15 14:31:08 -0500

    Updating style3.css and adding in script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openMenu() {
+  document.querySelector(".modal").classList.add("modal--open");
+}
+ 
+function closeMenu() {
+  document.querySelector(".modal").classList.remove("modal--open");
+}
